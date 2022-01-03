ff445a1b4baee1b1152d444c52440469a10e9dd1
HmndTyphn <Jared.elliott09@gmail.com>
2022-01-03 11:14:47 -0600

    install dep

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 // install dependencies
 const inquirer = require("inquirer");
+const fs= require('inquirer');
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
+const { mngQuestions, teamQuestions, engQuestions, intQuestions } = require('./src/questions');
+const renderHtml = require('./src/renderHtml');
+
+// empty array for team object
+const teamObject = [];
+
+// starts program when running node index.js
+
 
